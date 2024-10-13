900f37a35f13c20a6d8785afe0de97368a113811
augetyz <2061477607@qq.com>
2024-10-13 16:57:46 +0800

    update

--- CtrBoard-H7_CTRL/App/move_task.c
+++ CtrBoard-H7_CTRL/App/move_task.c
@@ -1,6 +1,11 @@
 #include "move_task.h"
 #include "cmsis_os.h"
 
+/**
+  * @brief  移动控制任务入口函数，用于处理机器人的移动控制
+  * @param  argument: 传递给任务的参数（未使用）
+  * @retval None
+  */
 void MoveTask_Entry(void const * argument)
 {
   /* Infinite loop */
